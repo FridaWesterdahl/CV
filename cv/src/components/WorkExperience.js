96a97b8f46492a4be2a8ff09415001e3e620f2b9
Frida <fridaawesterdahl@gmail.com>
2022-04-11 22:07:50 +0200

    fixed some responsivity

--- cv/src/components/WorkExperience.js
+++ cv/src/components/WorkExperience.js
@@ -20,44 +20,46 @@ function WorkExperience() {
     return (
         <div className='work-ex'>
             <h1>WORK EXPERIENCE</h1>
-            <ul>
-                <li>
-                    <h2>Espresso House, barista</h2>
-                    <h3>2021-10-01 - ongoing</h3>
-                    <p className={click1 ? 'info' : 'hidden'}>Today I am working extra as a barista.
-                        Serving customers and giving them their best possible coffee experience!</p>
-                    <button className={click1 ? 'close' : 'show-more'} onClick={handleClick1}></button>
-                </li>
-                <li>
-                    <h2>Espresso House, coffeeshop manager</h2>
-                    <h3>2019-10-01 - 2021-09-30</h3>
-                    <p className={click2 ? 'info' : 'hidden'}>Management of two different coffeeshops
-                        of different sizes and revenue. Handleing a staff group up to 14 people with
-                        development talks in the daily work and more formal sit downs.</p>
-                    <button className={click2 ? 'close' : 'show-more'} onClick={handleClick2}></button>
-                </li>
-                <li>
-                    <h2>Nordic Wellness, receptionist</h2>
-                    <h3>2018-09-01 - 2019-09-30</h3>
-                    <p className={click3 ? 'info' : 'hidden'}>Customer service and taking care of the
-                        gym and equipment.</p>
-                    <button className={click3 ? 'close' : 'show-more'} onClick={handleClick3}></button>
-                </li>
-                <li>
-                    <h2>McDonald's, shiftleader</h2>
-                    <h3>2017-04-01 - 2018-08-31</h3>
-                    <p className={click4 ? 'info' : 'hidden'}>Leadership during the shift and plan
-                        the staffing.</p>
-                    <button className={click4 ? 'close' : 'show-more'} onClick={handleClick4}></button>
-                </li>
-                <li>
-                    <h2>Apoteket Hjärtat, in-/outbound</h2>
-                    <h3>2015-09-01 - 2017-02-31</h3>
-                    <p className={click5 ? 'info' : 'hidden'}>Working at Apoteket Hjärtats warehouse
-                        with both inbound and outbound.</p>
-                    <button className={click5 ? 'close' : 'show-more'} onClick={handleClick5}></button>
-                </li>
-            </ul>
+            <div id="workex-container">
+                <ul>
+                    <li>
+                        <h2>Espresso House, barista</h2>
+                        <h3>2021-10-01 - ongoing</h3>
+                        <p className={click1 ? 'info' : 'hidden'}>Today I am working extra as a barista.
+                            Serving customers and giving them their best possible coffee experience!</p>
+                        <button className={click1 ? 'close' : 'show-more'} onClick={handleClick1}></button>
+                    </li>
+                    <li>
+                        <h2>Espresso House, coffeeshop manager</h2>
+                        <h3>2019-10-01 - 2021-09-30</h3>
+                        <p className={click2 ? 'info' : 'hidden'}>Management of two different coffeeshops
+                            of different sizes and revenue. Handleing a staff group up to 14 people with
+                            development talks in the daily work and more formal sit downs.</p>
+                        <button className={click2 ? 'close' : 'show-more'} onClick={handleClick2}></button>
+                    </li>
+                    <li>
+                        <h2>Nordic Wellness, receptionist</h2>
+                        <h3>2018-09-01 - 2019-09-30</h3>
+                        <p className={click3 ? 'info' : 'hidden'}>Customer service and taking care of the
+                            gym and equipment.</p>
+                        <button className={click3 ? 'close' : 'show-more'} onClick={handleClick3}></button>
+                    </li>
+                    <li>
+                        <h2>McDonald's, shiftleader</h2>
+                        <h3>2017-04-01 - 2018-08-31</h3>
+                        <p className={click4 ? 'info' : 'hidden'}>Leadership during the shift and plan
+                            the staffing.</p>
+                        <button className={click4 ? 'close' : 'show-more'} onClick={handleClick4}></button>
+                    </li>
+                    <li>
+                        <h2>Apoteket Hjärtat, in-/outbound</h2>
+                        <h3>2015-09-01 - 2017-02-31</h3>
+                        <p className={click5 ? 'info' : 'hidden'}>Working at Apoteket Hjärtats warehouse
+                            with both inbound and outbound.</p>
+                        <button className={click5 ? 'close' : 'show-more'} onClick={handleClick5}></button>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
